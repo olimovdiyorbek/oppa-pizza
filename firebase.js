57d0f826e88ae8adb4ab7dd4497a8cabb8d41e5b
Diyorbek Olimov <olimovdiyorbek2009@gmail.com>
2026-03-08 11:37:13 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,1 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT.appspot.com",
-  messagingSenderId: "200919",
-  appId: "200919"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
